e64d7767ff2b6faca65874b01dbc19a6199afc9d
Hee Choi <35519824+heec12@users.noreply.github.com>
2020-02-06 12:38:02 -0500

    init
    initial uploading

---
+++ README.md
@@ -0,0 +1,21 @@
+# Project Title
+
+The Self-evaluation checklists are designed to assist Ph.D. advisors to improve advising their students. This code solves the complex equations of human interactions. After the problem is simulated, it determines how good you are at advising your students. 
+
+## Running program
+
+It is python-based program written specifically for one type simulation. Although, the same capability could be extended for multiple problems in future with enough support (time) for this project.
+
+The source code can be used simply by typing on the terminal screen:
+
+```
+python sea.py
+```
+
+In the current version, it is assumed that the user has Python 3 installed on their system (if user is still using Python 2.x.x, they must be old-school and refrain from using this program). The program must be in the current directory from where it is called. 
+
+## Authors
+
+* **Hee Choi**
+* **Alex Thames** 
+
---
+++ sea.py
@@ -0,0 +1,51 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import time
+import os
+import sys
+
+currentdir = os.path.realpath(__file__)
+downloadsdir = currentdir[:-len(__file__)]
+
+parameters = {
+    "Support": 
+        'You want to help people directly, either individually or in groups.',
+    "Availability": 
+        'You attempt to meet with students regularly, setting aside adequate and comfortable time for students.',
+    "Interest and Enthusiasm": 
+        'You are interested and enthusiastic about your own and the student\'s work.',
+    "Knowledge and Expertise in the Field": 
+        'You feel some mastery in your field and use your knowledge to understand and demonstrate how the student\'s research topic fits within the wider field.',
+    "Interest in the Student's Career":
+        'You provide ample support for establishing the student\'s career in various ways.',
+    "Good Communication":
+        'You have good listening skills, communicate in an honest and fair manner about issues that arise, and make expectations clear.',
+    "Constructive Feedback":
+        'You provide feedback and criticism of student\'s work that is constructive and prompt.',
+    "Provides Direction and Structure":
+        'You provide an appropriate amount of direction and structure to the student\'s research project.',
+    "Approachabillity and Rapport":
+        'You are approachable and work to establish a good rapport with your students',
+    }
+
+for key in parameters:
+    print("\n\n\nPLEASE ENTER HOW MUCH YOU AGREE WITH THESE STATEMENTS (IN NUMBER):\n")
+    print("-----------------------------------------------------------------------------------")
+    print(key, ":\n", parameters[key])
+    print("-----------------------------------------------------------------------------------")
+    
+    num_value = input("\nVery High(1), High(2), Moderate(3), Not at all(4)\n")
+
+print ("\n\nCALCULATING RESULTS\n\n")
+toolbar_width = 80
+for i in range(toolbar_width):
+    time.sleep(0.1)
+    sys.stdout.write("*")
+    sys.stdout.flush()
+
+sys.stdout.write("\n")
+
+img=mpimg.imread(downloadsdir + 'output.png')
+imgplot = plt.imshow(img)
+plt.axis('off')
+plt.show()
